d81dc028fa2ba13b651cf13aa5ab2c5b02d6a96b
Dhaval Savalia <dhaval.savalia6@gmail.com>
2019-02-06 18:05:04 +0530

    use pipenv and make project more functionalga

--- main.py
+++ main.py
@@ -1,210 +1,207 @@
-import peewee
 from peewee import *
+import datetime
+import sys
+import os
 
-db = peewee.SqliteDatabase('library.db')
+DATABASE = 'library.db'
 
-class book(Model):																#Table for Books
-	title = CharField()
-	author = CharField()
-	publication = CharField()
-	pub_year = IntegerField()
-	isbn = CharField()
-	num_of_books = IntegerField()
-
-	class Meta:
-		database = db
+db = SqliteDatabase(DATABASE)
 
 
-class member(Model):															#Table for members
-	user_id = CharField()
-	name = CharField()
-	phone_no = CharField()
+class Book(Model):
+    title = CharField()
+    author = CharField()
+    publication = CharField()
+    pub_year = IntegerField()
+    isbn = CharField()
+    num_of_books = IntegerField()
 
-	class Meta:
-		database = db
-
-class issue_history(Model):														#Table for issue handling
-	user_id = ForeignKeyField(member, related_name='pets')
-	isbn = ForeignKeyField(book, related_name='library')
-	issue_id = CharField()
-	issue_date = DateField()
-	return_date = DateField()
-	current_status = TextField()
-
-	class Meta:
-		database = db
+    class Meta:
+        database = db
 
 
-def initialize_db():															#function to connect to db
-	db.connect()
-	try:
-		db.create_tables([book, member, issue_history])
-	except OperationalError:
-		# Table already exists. Do nothing
-		pass
+class Member(Model):
+    user_id = CharField()
+    name = CharField()
+    phone_no = CharField()
 
-def deinit():																	#func to close connection to db
-	db.close()
+    class Meta:
+        database = db
 
 
-def add_book():																	#func to enter a new book
-	book_title = raw_input('Enter the book title: ')
-	book_author = raw_input('Enter the book author: ')
-	book_publication = raw_input('Enter the book publication: ')
-	book_pub_year = raw_input('Enter the year of publication of book: ')
-	book_isbn = raw_input('Enter the ISBN code of book: ')
-	#book_no = number of books having same ISBN
-	
+class IssueHistory(Model):
+    user_id = ForeignKeyField(Member, related_name='pets')
+    isbn = ForeignKeyField(Book, related_name='library')
+    issue_id = CharField()
+    issue_date = DateField(default=datetime.datetime.now)
+    return_date = DateField()
+    current_status = TextField()
 
-def add_book_to_db(comment, TableName=book):									#func to actually save books to db
-	book_data = book(title=book_title,
-						author=book_author,
-						publication=book_publication,
-						pub_year=book_pub_year,
-						isbn=book_isbn,
-						current_status=current_status)
-	book_data.save()
+    class Meta:
+        database = db
 
 
-def add_member():																#func to add a new member
-	user_id = raw_input('Enter the user_id: ')
-	member_name = raw_input('Enter the member name: ')
-	member_phone_no = raw_input('Enter the phone number of the member: ')
+def create_tables():
+    with db:
+        db.create_tables([Book, Member, IssueHistory])
 
 
-def add_member_to_db(comment, TableName=member):								#func to actually save a new member to db
-	member_data = book(user_id=user_id,
-						name=member_name,
-						phone_no=member_phone_no)
-	book_data.save()
+def deinit():
+    db.close()
 
 
-def allocate():																	#func to allocate a book
-	alloc_book_isbn = raw_input('Enter book isbn to be allocated: ')
-	#check alloc_book_id with actual book id in database
-	if alloc_book_id in book_isbn:
-		alloc_member_id = raw_input('Enter member id receving the book:')
-		#checks if correct user_id is entered
-		if alloc_member_id in user_id:
-			issue_date = raw_input('Enter issueing date:')
-			return_date = raw_input('Enter return date:')
-			#checks if date is properly entered or not
-			if start_date > return_date:
-				print ('Issueing date cannot be after return date')
-				#go to line 37
-			
-			else :
-				issue_id = issue_date + user_id + return_date 
-				issue_data.save()
-				issue.append(sr_no)
-				current_status = 'issued'
-				print 'This is your issue id: {0}'.format(issue_id) 
-				print 'Your return date is: {0}'.format(return_date)
+def add_book():
 
-		else:
-			print ('user_id entered does not exist')
-			#go to line line 35
-		
-	else:
-		print ('ISBN entered is not present in the database')
-		#go to line 32
-	
-
-def save_allocate_to_db(comment, TableName=issue_history):								#func to save the new allocation data to db
-	issue_data = book(user_id=alloc_member_id,
-						isbn=alloc_book_isbn,
-						issue_id=issue_id,
-						issue_date=issue_date,
-						return_date=return_date,
-						num_of_books=book_no)
-	issue_data.save()
+    book_data = Book(
+        title=input('Enter the book title: '),
+        author=input('Enter the book author: '),
+        publication=input('Enter the book publication: '),
+        pub_year=input('Enter the year of publication of book: '),
+        isbn=input('Enter the ISBN code of book: '),
+        num_of_books=input("Enter the number of books having same ISBN: ")
+    )
+    book_data.save()
+    os.system('clear')
 
 
-def de_allocate():																#func to de-allocate a book
-	de_alloc_issue_id = raw_input('Enter the issue id provided to you:')
-	#check alloc_book_id with actual book id in database
-	if de_alloc_issue_id in issue_data:
-		if today_date == return_date in issue_data:
-			print ('Book received')
-			book_no += 1
-			current_status = 'returned'
+def add_member():
 
-		elif today_date < return_date:
-			overdue_days = return_date - today_date
-			fine = 5*overdue_days
-			print "Your fine is Rs", fine
-			current_status = 'returned'
-
-		else:
-			book_no+= 1
-			print 'Thank you for choosing Python Library'
-
-#def update_de_allocate_to_db():
-	#function to add returned status to book
+    member_data = Member(
+        user_id=input('Enter the user_id: '),
+        name=input('Enter the member name: '),
+        phone_no=input('Enter the phone number of the member: ')
+    )
+    member_data.save()
+    os.system('clear')
 
 
-def remove_book():																
-	check_isbn = raw_input('Enter the ISBN code of the book you want to remove: ')
-	
-	for check_isbn in book_isbn(1,10):
-		#remove book action
-		book = book.get(book_isbn == check_isbn)
-		book.delete_instance()
+def allocate():
 
-		print 'The book',check_isbn,'has been deleted.'
-	
+    alloc_book_isbn = input('Enter book isbn to be allocated: ')
+    if Book.get(Book.isbn == alloc_book_isbn):
+        alloc_member_id = input('Enter member id: ')
+        if Member.get(Member.user_id == alloc_member_id):
+            return_date = input('Enter a date in YYYY-MM-DD format: ')
+            year, month, day = map(int, return_date.split('-'))
+            return_date = datetime.date(year, month, day)
 
+            if datetime.datetime.now().date() > return_date:
+                print('Issueing date cannot be after return date')
 
+            else:
+                issue_date = datetime.datetime.now().date()
+                issue_id = str(issue_date) + alloc_member_id + str(return_date)
+                current_status = 'issued'
+                issue_data = IssueHistory(
+                    user_id=alloc_member_id,
+                    isbn=alloc_book_isbn,
+                    issue_id=issue_id,
+                    issue_date=issue_date,
+                    return_date=return_date,
+                    current_status=current_status
+                )
+                issue_data.save()
+                print('This is your issue id: {0}'.format(issue_id))
+                print('Your return date is: {0}'.format(return_date))
 
-def remove_member():															#func to remove member from db
-	rem_member_id = raw_input("Enter member's user_id : ")
-	# remove member action
-	member = member.get(user_id == rem_member_id)
-	member.delete_instance()
+        else:
+            print('user_id entered does not exist')
 
-	print 'Member',rem_member_id,'has been removed.'
-	
-
-
-
-print """							  		
-	 	Welcome to Python Library System.
-	  	a To add a book
-	  	b To add a member
-	  	c To allocate a book
-	 	d To return a book
-	 	e To remove a book from the collection
-	  	f To remove a member from the book
-	 """
-
-user_option = raw_input('Enter your choice now:')
-user_option = user_option.lower()
-
+    else:
+        print('ISBN entered is not present in the database')
+    os.system('clear')
 
 
+def de_allocate():
+
+    de_alloc_user_id = input('Enter your user id: ')
+    if IssueHistory.get(IssueHistory.user_id == de_alloc_user_id):
+        issue_data = IssueHistory.get(IssueHistory.user_id == de_alloc_user_id)
+        today_date = datetime.datetime.now().date()
+        if today_date <= issue_data.return_date:
+            print('Book received')
+            current_status = 'returned'
+
+        elif today_date > issue_data.return_date:
+            overdue_days = issue_data.return_date - today_date
+            fine = 5 * overdue_days
+            print("Your fine is Rs.", fine)
+            current_status = 'returned'
+
+        else:
+            print('This shouldn\'t be happening')
+    os.system('clear')
 
 
-if user_option == 'a':
-	add_book()
+def remove_book():
 
-elif user_option == 'b':
-	add_member()
-	
-elif user_option == 'c':
-	allocate()
-	
-elif user_option == 'd':
-	de_allocate()
-	
-elif user_option == 'e':
-	remove_book()
-	
-elif user_option == 'f':
-	remove_member()
-else:
-	print "Invalid choice"
+    book_isbn = input('Enter the ISBN code of the book you want to remove: ')
+
+    book = Book.get(Book.isbn == book_isbn)
+    book.delete_instance()
+
+    print('The book with ISBN', book_isbn, 'has been deleted.')
+    os.system('clear')
+
+
+def remove_member():
+
+    rem_member_id = input("Enter member's user id: ")
+    member = Member.get(Member.user_id == rem_member_id)
+    member.delete_instance()
+
+    print('Member', rem_member_id, 'has been removed.')
+    os.system('clear')
+
+
+def main():
+    print("""
+            Welcome to Python Library System:
+            (a) To add a book
+            (b) To add a member
+            (c) To allocate a book
+            (d) To return a book
+            (e) To remove a book from the collection
+            (f) To remove a member from the library
+    """)
+
+    user_option = input('Enter your choice now: ')
+    user_option = user_option.lower()
+
+    if user_option == 'a':
+        add_book()
+
+    elif user_option == 'b':
+        add_member()
+
+    elif user_option == 'c':
+        allocate()
+
+    elif user_option == 'd':
+        de_allocate()
+
+    elif user_option == 'e':
+        remove_book()
+
+    elif user_option == 'f':
+        remove_member()
+
+    elif user_option == 'x':
+        print('\nThank you for using Python Library System!')
+        sys.exit()
+
+    else:
+        print("Invalid choice")
 
 if __name__ == '__main__':
-	initialize_db()
-	
-
+    try:
+        create_tables()
+        while (True):
+            main()
+    except KeyboardInterrupt:
+        print('\nThank you for using Python Library System!')
+        db.close()
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
